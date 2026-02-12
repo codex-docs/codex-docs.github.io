0e3ebd61fbb0481035e16a5aac62ba0cad18f57a
manman.li <manman.li@mihoyo.com>
2026-02-12 23:34:53 +0800

    add google ads script

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1770909910',resources: [
+    cacheName: 'chirpy-1770910356',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
